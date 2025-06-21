4f5658bc841bafe6c0cf4877b84b32482b4bb608
Shreyansh V <shreyansh6410@gmail.com>
2025-06-21 21:00:57 +0530

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+let colors = [" #00BFFF", " #DC143C", " #32CD32", " #FFD700", "	#DA70D6"];
 
 function App() {
+  const [ColorIndex, SetColorIndex] = useState(0);
+  const ChangeColor = () => {
+    SetColorIndex(
+      (prevColorIndex: number) => (prevColorIndex + 1) % colors.length
+    );
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="d-flex justify-content-center" style={{ height: "100vh" }}>
+      <button
+        type="button"
+        className="btn"
+        onClick={ChangeColor}
+        style={{
+          background: colors[ColorIndex],
+        }}
+      >
+        Base class
+      </button>
     </div>
   );
 }
